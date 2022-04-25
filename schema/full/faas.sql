4a6c1117feb2699062e6f55b029a222d475112a8
vvlgo <29571935@qq.com>
2022-04-25 15:54:52 +0800

    docs: update sql

--- schema/full/faas.sql
+++ schema/full/faas.sql
@@ -1,4 +1,4 @@
-create table faas.dockers
+create table dockers
 (
     id         varchar(64)  not null
         primary key,
@@ -16,16 +16,15 @@ create table faas.dockers
     tenant_id  varchar(64)  null
 );
 
-create table faas.functions
+create table functions
 (
     id           varchar(64)  not null
         primary key,
-    group_name   varchar(200) null,
-    project      varchar(200) null,
+    group_id     varchar(200) null,
+    project_id   varchar(200) null,
     version      varchar(200) null,
     language     varchar(200) null,
     status       varchar(200) null,
-    doc          int          COMMENT 'status of function doc',
     env          text         null,
     created_at   bigint       null,
     updated_at   bigint       null,
@@ -40,19 +39,23 @@ create table faas.functions
         unique (name)
 );
 
-create table faas.gits
+create table gits
 (
-    id         varchar(64)  not null
+    id          varchar(64)  not null
         primary key,
-    host       varchar(200) null,
-    token      text         null,
-    name       varchar(200) null,
-    created_at bigint       null,
-    updated_at bigint       null,
-    deleted_at bigint       null,
-    created_by varchar(64)  null,
-    updated_by varchar(64)  null,
-    deleted_by varchar(64)  null,
-    tenant_id  varchar(64)  null
+    host        varchar(200) null,
+    token       text         null,
+    name        varchar(200) null,
+    created_at  bigint       null,
+    updated_at  bigint       null,
+    deleted_at  bigint       null,
+    created_by  varchar(64)  null,
+    updated_by  varchar(64)  null,
+    deleted_by  varchar(64)  null,
+    tenant_id   varchar(64)  null,
+    ssh         text         null,
+    known_hosts text         null
 );
+
+
 
